440c2c962d3110de1c65d86a58a470e096c38af4
Pawel Bialas <p.cz.bialas@gmail.com>
2018-12-16 13:42:18 +0100

    cities in one country

--- src/main/java/world/Excercise/QuerryCountryWithLessCities.java
+++ src/main/java/world/Excercise/QuerryCountryWithLessCities.java
@@ -7,13 +7,13 @@ import org.hibernate.query.Query;
 
 public class QuerryCountryWithLessCities {
 
-//    public static void main(String[] args) {
-//        try(SessionFactory factory = new Configuration().configure().buildSessionFactory();
-//            Session sess = factory.openSession()) {
-//            Query<Object[]> q = sess.createQuery("SELECT c.name, size(c.cities) " +
-//                    " FROM Country c GROUP BY c");
-//            q.stream().filter(o -> o[1]<2).forEach(
-//                    o -> System.out.println(o[0] + " " + o[1]));
-//        }
-//    }
+    public static void main(String[] args) {
+        try(SessionFactory factory = new Configuration().configure().buildSessionFactory();
+            Session sess = factory.openSession()) {
+            Query<Object[]> q = sess.createQuery("SELECT c.name, size(c.cities) " +
+                    " FROM Country c GROUP BY c");
+            q.stream().forEach(
+                    o -> System.out.println(o[0] + " " + o[1]));
+        }
+    }
 }
