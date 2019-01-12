fd012fa3bbbf1415e12b88a986287a62a7663b10
Pawel Bialas <p.cz.bialas@gmail.com>
2019-01-12 09:23:46 +0100

    next exc

---
+++ src/main/java/world/Excercise/Exc1.java
@@ -0,0 +1,22 @@
+package world.Excercise;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import world.model.City;
+import world.model.Country;
+
+public class Exc1 {
+
+    public static void main(String[] args) {
+        try(SessionFactory factory = new Configuration().configure().buildSessionFactory();
+            Session sess = factory.openSession()) {
+            City city = sess.load(City.class,1);
+            Transaction tx = sess.beginTransaction();
+            city.setDistrict("ABC");
+            tx.commit();
+        }
+    }
+}
